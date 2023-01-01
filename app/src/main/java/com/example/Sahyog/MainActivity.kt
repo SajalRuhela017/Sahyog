abeaa06104a38405fcffa6ef47d164cdd36ee80d
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-01-01 21:22:38 +0530

    Sixth Commit

--- app/src/main/java/com/example/Sahyog/MainActivity.kt
+++ app/src/main/java/com/example/Sahyog/MainActivity.kt
@@ -81,17 +81,10 @@ class MainActivity : AppCompatActivity() {
     private fun getCurrentUser() {
         val user = Firebase.auth.currentUser
         user?.let {
-            // Name, email address, and profile photo Url
             val name = user.displayName
             val email = user.email
             val photoUrl = user.photoUrl
-
-            // Check if user's email is verified
             val emailVerified = user.isEmailVerified
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getToken() instead.
             val uid = user.uid
         }
     }
